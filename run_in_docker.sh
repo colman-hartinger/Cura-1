a6eca433b393b13b59da97c797c4f984d2c807c8
colman-hartinger <62404177+colman-hartinger@users.noreply.github.com>
2020-10-01 15:23:09 -0600

    Update run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,4 +2,6 @@
 Xvfb :1.0 -screen 0 1280x800x16 &
 export DISPLAY=:1.0
 export QT_QPA_PLATFORM=offscreen
-python3 cura_app.py --headless
+python3 plugins/SmartSlicePlugin/tests/run.py
+
+#python3 cura_app.py --headless
